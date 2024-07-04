71bacff637e65dcaf51b2e14734649bb8677fd44
xxryhnar05 <145762941+xxryhnar05@users.noreply.github.com>
2024-07-04 22:57:52 +0700

    first commit

---
+++ src/pet.java
@@ -0,0 +1,15 @@
+public class pet {
+    String nama = "Jefri";
+    int umur = 11;
+    String ras = "Draco";
+    String makanan = "insect";
+    String warna = "Kadal";
+
+    public void displayPet() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Ras: " + ras);
+        System.out.println("Makanan: " + makanan);
+        System.out.println("Warna: " + warna);
+    }
+}
